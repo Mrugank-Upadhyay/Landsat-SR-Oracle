129466b4fa25da8c802734753ff86fe5619201ac
Mrugank Upadhyay <mrugank2@gmail.com>
2024-10-11 00:27:38 -0400

    Added Satellite Resolution type and Scene Search type for storing scene data

--- src/app/store/globalStore.ts
+++ src/app/store/globalStore.ts
@@ -3,6 +3,78 @@ import { PathRow } from '../map_page';
 import { LngLat } from 'mapbox-gl';
 import { Acquisition } from '../acquisitions_table';
 
+
+export type SatelliteResolution = {
+    [satellite: string]: {
+            bandNum: number,
+            bandName: string, 
+            bandCategory: string, // (Visible, Near-Infrared, Infrared, Panchromatic, Cirrus, Thermal-Infrared)
+            bandResolution: number // In meters (m)
+        }[]
+}
+
+export interface SceneSearch {
+    browse: Browse[]
+    cloudCover: number
+    entityId: string
+    displayId: string
+    orderingId: any
+    metadata: Metadaum[]
+    hasCustomizedMetadata: any
+    options: Options
+    selected: Selected
+    spatialBounds: SpatialBounds
+    spatialCoverage: SpatialCoverage
+    temporalCoverage: TemporalCoverage
+    publishDate: string
+  }
+  
+  export interface Browse {
+    id: string
+    browseRotationEnabled: any
+    browseName: string
+    browsePath: string
+    overlayPath: string
+    overlayType: string
+    thumbnailPath: string
+  }
+  
+  export interface Metadaum {
+    id: string
+    fieldName: string
+    dictionaryLink: string
+    value: any
+  }
+  
+  export interface Options {
+    bulk: boolean
+    download: boolean
+    order: boolean
+    secondary: boolean
+  }
+  
+  export interface Selected {
+    bulk: boolean
+    compare: boolean
+    order: boolean
+  }
+  
+  export interface SpatialBounds {
+    type: string
+    coordinates: number[][][]
+  }
+  
+  export interface SpatialCoverage {
+    type: string
+    coordinates: number[][][]
+  }
+  
+  export interface TemporalCoverage {
+    endDate: string
+    startDate: string
+  }
+  
+
 export interface GlobalState {
     pathRows: PathRow[],
     acquisitions: Acquisition[], // All acquisitions (total 6, DESC order)
@@ -10,14 +82,15 @@ export interface GlobalState {
     notificationLeadTime?: Date,
     cloudCoverThreshold?: number, // cloud cover under this value (inclusive),
     chosenAcquisition?: Acquisition,
-    satelliteResolution?: number,
+    satelliteResolution: SatelliteResolution,
+    sceneSearch?: SceneSearch[],
     updatePathRows: (val: PathRow[]) => void, // I ain't explaining all of this, good luck, I'm tired
     updateAcquisitions: (val: Acquisition[]) => void,
     updateMarkerLngLat: (val: LngLat | null) => void,
     updateNotificationLeadTime: (val: Date) => void,
     updateCloudCoverThreshold: (val: number) => void,
     updateChosenAcquisition: (val: Acquisition) => void,
-    updateSatelliteResolution: (val: number) => void,
+    updateSceneSearch: (val: SceneSearch[]) => void,
 }
 
 export const useGlobalStore =  create<GlobalState>()((set) => ({
@@ -25,11 +98,40 @@ export const useGlobalStore =  create<GlobalState>()((set) => ({
     acquisitions: [],
     cloudCoverThreshold: 1,
     markerLngLat: null,
+    satelliteResolution: {
+        "Landsat-8": [
+            { bandNum: 1, bandName: "Visible Coastal Aerosol", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 2, bandName: "Visible Blue", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 3, bandName: "Visible Green", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 4, bandName: "Visible Red", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 5, bandName: "Near-Infrared", bandCategory: "Near-Infrared", bandResolution: 30 },
+            { bandNum: 6, bandName: "Shortwave-Infrared 1", bandCategory: "Infrared", bandResolution: 30 },
+            { bandNum: 7, bandName: "Shortwave-Infrared 2", bandCategory: "Infrared", bandResolution: 30 },
+            { bandNum: 8, bandName: "Panchromatic (PAN)", bandCategory: "Panchromatic", bandResolution: 15 },
+            { bandNum: 9, bandName: "Cirrus", bandCategory: "Cirrus", bandResolution: 30 },
+            { bandNum: 10, bandName: "Thermal-Infrared Sensor 1", bandCategory: "Thermal-Infrared", bandResolution: 100 },
+            { bandNum: 11, bandName: "Thermal-Infrared Sensor 2", bandCategory: "Thermal-Infrared", bandResolution: 100 },
+
+        ],
+        "Landsat-9": [
+            { bandNum: 1, bandName: "Visible Coastal Aerosol", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 2, bandName: "Visible Blue", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 3, bandName: "Visible Green", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 4, bandName: "Visible Red", bandCategory: "Visible", bandResolution: 30 },
+            { bandNum: 5, bandName: "Near-Infrared", bandCategory: "Near-Infrared", bandResolution: 30 },
+            { bandNum: 6, bandName: "Shortwave-Infrared 1", bandCategory: "Infrared", bandResolution: 30 },
+            { bandNum: 7, bandName: "Shortwave-Infrared 2", bandCategory: "Infrared", bandResolution: 30 },
+            { bandNum: 8, bandName: "Panchromatic (PAN)", bandCategory: "Panchromatic", bandResolution: 15 },
+            { bandNum: 9, bandName: "Cirrus", bandCategory: "Cirrus", bandResolution: 30 },
+            { bandNum: 10, bandName: "Thermal-Infrared Sensor 1", bandCategory: "Thermal-Infrared", bandResolution: 100 },
+            { bandNum: 11, bandName: "Thermal-Infrared Sensor 2", bandCategory: "Thermal-Infrared", bandResolution: 100 },
+        ]
+    },
     updatePathRows: (val: PathRow[]) => set({pathRows: val}),
     updateAcquisitions: (val: Acquisition[]) => set({acquisitions: val}),
     updateMarkerLngLat: (val: LngLat | null) => set({markerLngLat: val}),
     updateNotificationLeadTime: (val: Date) => set({notificationLeadTime: val}),
     updateCloudCoverThreshold: (val: number) => set({cloudCoverThreshold: val}),
     updateChosenAcquisition: (val: Acquisition) => set({chosenAcquisition: val}),
-    updateSatelliteResolution: (val: number) => set({satelliteResolution: val}),
+    updateSceneSearch: (val: SceneSearch[]) => set({sceneSearch: val})
 }))
