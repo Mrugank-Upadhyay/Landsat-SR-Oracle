7ffd3baee31c93810ce090bd61eac3bb56ff15e4
mihranmashhud <mihranmashhud@gmail.com>
2024-10-10 23:22:12 -0400

    Add middleware and simplify scene-search API.

--- src/app/api/scene-search/route.ts
+++ src/app/api/scene-search/route.ts
@@ -1,20 +1,58 @@
 export type SceneSearchResponse = {
-    data: {results: any[]},
-    errorCode: string,
-    errorMessage: string,
+  data: { results: any[] },
+  errorCode: string,
+  errorMessage: string,
+}
+
+enum WRSFilterID {
+  WRSPathFilterId = "5e83d14fb9436d88",
+  WRSRowFilterId = "5e83d14ff1eda1b8"
 }
 
 // We need to capture the request to the USGS Scene-Search as a POST request and then refetch it as GET
 // Since routes in Next.js can't use json bodies outside of POST methods.
 export async function POST(request: Request) {
-    const req = await request.json();
-    const {url, headers, ...rest} = req
+  const {
+    path,
+    row,
+    cloudMax,
+  } = await request.json();
 
-    const res = await fetch(url, {
-        headers: headers,
-        method: "POST",
-        body: JSON.stringify(rest)
-    });
+  const url = (process.env.M2M_API_URL || "") + "/scene-search"
+  const loginToken = request.headers.get("X-Auth-Token") || ""
 
-    return res
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      "X-Auth-Token": loginToken,
+    },
+    body: JSON.stringify({
+      datasetName: "landsat_ot_c2_l2",
+      maxResults: 5,
+      metadataType: "full",
+      sceneFilter: {
+        metadataFilter: {
+          filterType: "and",
+          childFilters: [
+            {
+              filterId: WRSFilterID.WRSPathFilterId,
+              filterType: "between",
+              firstValue: path,
+              secondValue: path
+            },
+            {
+              filterId: WRSFilterID.WRSRowFilterId,
+              filterType: "between",
+              firstValue: row,
+              secondValue: row
+            }
+          ]
+        },
+        cloudCoverFilter: {
+          min: 0,
+          max: cloudMax
+        }
+      }
+    })
+  });
 }
---
+++ src/app/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const m2m_api_paths = [
+  "/api/scene-search"
+]
+
+export async function middleware(request: NextRequest) {
+  if (m2m_api_paths.some(path => request.nextUrl.pathname.startsWith(path))) {
+    const url = (process.env.M2M_API_URL || "") + "/login-token"
+    const resp = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        username: process.env.M2M_USERNAME,
+        token: process.env.M2M_APP_TOKEN,
+      }),
+      next: {
+        revalidate: 60 * 60 * 2 - 60 // 1H:59M
+      }
+    })
+    const {
+      data: loginToken
+    } = await resp.json()
+    const headers = new Headers(request.headers)
+    headers.set("X-Auth-Token", loginToken)
+    return NextResponse.next({
+      request: {
+        headers,
+      }
+    })
+  }
+  return NextResponse.next()
+}
