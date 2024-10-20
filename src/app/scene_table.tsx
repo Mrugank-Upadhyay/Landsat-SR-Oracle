ca41f8495db3c561edd19d705884a688a562bdfa
Mrugank Upadhyay <mrugank2@gmail.com>
2024-10-20 17:24:51 -0400

    Stylized images and actually got them to show up

--- src/app/scene_table.tsx
+++ src/app/scene_table.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SceneSearchImage, useGlobalStore } from "./store/globalStore";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 export default function ScenesViewer() {
 
@@ -22,23 +23,34 @@ export default function ScenesViewer() {
       </div>
       <TabsContent value="Images">
         <Card>
-          <CardContent>
-            <div className="grid-col-2 col-span-1">
-              {
-                scenes
-                ? scenes.filter((scene) => sceneFilter(scene)).map((scene) => <img src={scene.browse[0].browsePath}></img>)
-                : <h2>
-                  No Images Found
-                </h2>
-              }
-            </div>
+          <CardContent className="p-2">
+              <ScrollArea className="h-full">
+                <div className="grid grid-cols-2 gap-2 pr-3 max-h-[42rem] overflow-y-scroll overflow-x-auto">
+                {
+                  scenes
+                  ? scenes.filter((scene) => sceneFilter(scene)).map((scene) => 
+                    <img 
+                      // TODO: How would this change if we use the query url for landsatlook instead of this browsePath?
+                        // Maybe set source depending on options selected (natural color would be default?)
+                      src={scene.browse[0].browsePath}
+                      // Add "Color Option {natural, infrared, single band X, etc} before 'Satellite Image'"
+                      alt={`Satellite Image for Path ${scene.metadata.find((metadatum) =>(metadatum.fieldName == "WRS Path") ? metadatum.value : false)} 
+                        Row ${scene.metadata.find((metadatum) => (metadatum.fieldName == "WRS Row") ? metadatum.value : false)}`}
+                      className="h-auto m-w-full rounded-lg"
+                    />)
+                  : <h2>
+                    No Images Found
+                  </h2>
+                }
+                </div>
+              </ScrollArea>
           </CardContent>
         </Card>
       </TabsContent>
       <TabsContent value="Options">
         <Card>
-          <CardContent>
-            <div className="">
+          <CardContent className="p-2">
+            <div className="h-full">
   
             </div>
           </CardContent>
