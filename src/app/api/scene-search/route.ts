45ec40e6c9552d5e2fdf5c5ef7856794dfb24c37
Mrugank Upadhyay <mrugank2@gmail.com>
2024-10-24 23:49:41 -0400

    added scene search with cloud cover and acquisition date filters. Still need to test acquisition date (doesn't seem to be doing it right)

--- src/app/api/scene-search/route.ts
+++ src/app/api/scene-search/route.ts
@@ -1,3 +1,5 @@
+import { DateRange } from "react-day-picker";
+
 export type SceneSearchResponse = {
   data: { results: any[] };
   errorCode: string;
@@ -12,10 +14,51 @@ enum WRSFilterID {
 // We need to capture the request to the USGS Scene-Search as a POST request and then refetch it as GET
 // Since routes in Next.js can't use json bodies outside of POST methods.
 export async function POST(request: Request) {
-  const { path, row, acquisitionDate, cloudCoverMin, cloudCoverMax } = await request.json();
+  const { 
+    path, 
+    row, 
+    acquisitionDate, 
+    cloudCover,
+  }: {
+    path: number,
+    row: number,
+    acquisitionDate: DateRange | undefined,
+    cloudCover: number,
+  } = await request.json();
 
   const url = (process.env.M2M_API_URL || "") + "/scene-search";
   const loginToken = request.headers.get("X-Auth-Token") || "";
+  const body = {
+    datasetName: "landsat_ot_c2_l2",
+    maxResults: 10,
+    metadataType: "full",
+    sceneFilter: {
+      metadataFilter: {
+        filterType: "and",
+        childFilters: [
+          {
+            filterId: WRSFilterID.WRSPathFilterId,
+            filterType: "between",
+            firstValue: path,
+            secondValue: path,
+          },
+          {
+            filterId: WRSFilterID.WRSRowFilterId,
+            filterType: "between",
+            firstValue: row,
+            secondValue: row,
+          },
+        ],
+      },
+      // TODO: remove if we're going to get 100 scenes, and then let users filter afterwards (to just reduce API calls for scenes)
+      cloudCoverFilter: {
+        max: cloudCover || 100,
+      },
+      ...(acquisitionDate && {acquisitionDate: { start: acquisitionDate.from!, ...(acquisitionDate.to && { end: acquisitionDate.to})}})
+    },
+  }
+
+  console.log(`body = ${JSON.stringify(body)}`)
   
   // TODO: Add error handling
 
@@ -24,34 +67,6 @@ export async function POST(request: Request) {
     headers: {
       "X-Auth-Token": loginToken,
     },
-    body: JSON.stringify({
-      datasetName: "landsat_ot_c2_l2",
-      maxResults: 10,
-      metadataType: "full",
-      sceneFilter: {
-        metadataFilter: {
-          filterType: "and",
-          childFilters: [
-            {
-              filterId: WRSFilterID.WRSPathFilterId,
-              filterType: "between",
-              firstValue: path,
-              secondValue: path,
-            },
-            {
-              filterId: WRSFilterID.WRSRowFilterId,
-              filterType: "between",
-              firstValue: row,
-              secondValue: row,
-            },
-          ],
-        },
-        // TODO: remove if we're going to get 100 scenes, and then let users filter afterwards (to just reduce API calls for scenes)
-        cloudCoverFilter: {
-          min: cloudCoverMin || 0,
-          max: cloudCoverMax || 100,
-        },
-      },
-    }),
+    body: JSON.stringify(body),
   });
 }
